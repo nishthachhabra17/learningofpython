0ac083cd861e0b694fa2c137da60c2fe3a762527
nishthachhabra17 <nishthachhabra2018@gmail.com>
2020-07-22 23:03:07 +0530

    here we solved arithmatic expressions.

---
+++ HelloWorld/arithmatic.py
@@ -0,0 +1,23 @@
+a = 15
+b = 3
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
+
+print()
+for i in range(1,a-b):
+    print(i)
+
+e = 12
+d = 24
+f = 36
+
+print(e+f+d)
+
+print()
+for i in range (2,e+d+f):
+    print(i)
