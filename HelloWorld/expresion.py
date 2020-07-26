f406de9e52e54cb15bf07c3589a0de7634efc9a7
nishthachhabra17 <nishthachhabra2018@gmail.com>
2020-07-26 12:02:06 +0530

    here in first file we explained expression and in second we learnt about indexing and negative indexing

---
+++ HelloWorld/expresion.py
@@ -0,0 +1,9 @@
+a = 23
+b = a + 17
+c = b//7
+d = c + 45
+e = d + 10
+
+print( a - b * c / d + e )
+print(((( a - 6 ) + 40 ) / 2 ) * 7)
+print(a * b + 34)
---
+++ HelloWorld/strings3.py
@@ -0,0 +1,29 @@
+lady = "Beautiful"
+
+print(lady)
+print(lady[1])
+print(lady[2])
+print(lady[4])
+
+parrot = 'Norwegien Ray'
+
+print(parrot[0])
+print(parrot[1])
+print(parrot[2])
+print(parrot[3])
+print(parrot[11])
+print(parrot[12])
+
+print(parrot[-13])
+print(parrot[-12])
+print(parrot[-11])
+print(parrot[-10])
+print(parrot[-2])
+print(parrot[-1])
+
+print(parrot[0-13])
+print(parrot[1-13])
+print(parrot[2-13])
+print(parrot[3-13])
+print(parrot[11-13])
+print(parrot[12-13])
