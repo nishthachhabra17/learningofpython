8ce9ac4b87778efd73e690553b60d35ac682d1ba
nishthachhabra17 <nishthachhabra2018@gmail.com>
2020-08-08 16:17:39 +0530

    Here we have discussed about sequence operators in python.

---
+++ HelloWorld/sequence_operators.py
@@ -0,0 +1,23 @@
+string1 = "Harsh "
+string2 = "is going "
+string3 = "for a "
+string4 = "picnic "
+string5 = "with his friends "
+
+print(string1 + string2 + string3 + string4 + string5)
+
+print("Harsh is going for a picnic with his friends")
+
+today = "friday"
+print("day" in today)
+print("fri" in today)
+print("wed" in today)
+
+parent = "renu"
+print("ren" in parent)
+print("renu" in parent)
+print("Sameer" in parent)
+
+print("Nishu " * 6)
+print("Nishu " * (6+8))
+print("Nishu " * 2 + "3")
